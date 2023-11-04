4814383899baa2d85a36c5f1fd0b89f2b565ea1a
fervillafane <ferv_87@hotmail.com>
2023-11-03 22:01:05 -0300

    inicio repo trabajo front

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
-console.log("Hola");
+//console.log("Hola");
 
+const ticket=200;
+
+let estudiante= ticket*0.2;
+console.log(estudiante);
+
+let trainee= ticket*0.5;
+console.log(trainee);
+
+let junior= ticket*0.8;
+console.log(junior);
+
+function saldoPagar(){
+
+    if (===estudiante) {
+        
+    }
+
+    return saldoPagar
+}
+console.log(saldoPagar)
+
+/*
+//let cantidad= parseInt(document.getElementById("cantidad").value);
+console.log(cantidad);
+
+let cantidad;
+
+function valorEstudiante(){
+    estudiante*cantidad;
+}
+console.log(valorEstudiante)
+
+*/
